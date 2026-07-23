588ac280b9398d1c93c61b5cf698bd8c52b389f4
amalnathk-dev-code <amalnath.bridgeon@gmail.com>
2026-07-23 10:35:46 +0530

    Time: 10 ms (30.25%), Space: 65.7 MB (5.17%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -5,9 +5,10 @@
 var isPalindrome = function(x) {
     if(x<0){
         return false
+    }else{
+        return x == String(Math.abs(x)).split("").reverse().join("");
     }
 
-    return x == String(Math.abs(x)).split("").reverse().join("");
 
 
 };
