9ee3c4ab4661fb2f496ee871394b4ba7e794915e
amalnathk-dev-code <amalnath.bridgeon@gmail.com>
2026-08-01 19:15:40 +0530

    Time: 0 ms (100%), Space: 55.8 MB (7.84%) - LeetHub

--- 2427-number-of-common-factors/2427-number-of-common-factors.js
+++ 2427-number-of-common-factors/2427-number-of-common-factors.js
@@ -5,18 +5,18 @@
  */
 var commonFactors = function (a, b) {
     let c = 0;
-    // for(let i=1;i<(a>b ? a : b); i++){
-    //     if(a%i==0 && b%i==0){
-    //         c++;
-    //     }
-    // }
-    // return c
-    let i = 1
-    while (i < (a > b ? a : b)) {
-        if (a % i == 0 && b % i == 0) {
+    for(let i=1;i<(a>b ? a : b); i++){
+        if(a%i==0 && b%i==0){
             c++;
         }
-        i++;
     }
-    return a==b ? c+1 : c;
+    return a == b ? c+1 : c
+    // let i = 1
+    // while (i < (a > b ? a : b)) {
+    //     if (a % i == 0 && b % i == 0) {
+    //         c++;
+    //     }
+    //     i++;
+    // }
+    // return a==b ? c+1 : c;
 };
