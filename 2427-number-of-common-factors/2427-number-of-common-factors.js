8263cbb86c2e311c18763400b59552f527962546
amalnathk-dev-code <amalnath.bridgeon@gmail.com>
2026-08-01 19:13:33 +0530

    Time: 1 ms (49.02%), Space: 55 MB (17.65%) - LeetHub

---
+++ 2427-number-of-common-factors/2427-number-of-common-factors.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+var commonFactors = function (a, b) {
+    let c = 0;
+    // for(let i=1;i<(a>b ? a : b); i++){
+    //     if(a%i==0 && b%i==0){
+    //         c++;
+    //     }
+    // }
+    // return c
+    let i = 1
+    while (i < (a > b ? a : b)) {
+        if (a % i == 0 && b % i == 0) {
+            c++;
+        }
+        i++;
+    }
+    return a==b ? c+1 : c;
+};
